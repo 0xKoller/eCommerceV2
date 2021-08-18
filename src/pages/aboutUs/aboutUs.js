d694f84a9b295427c4b9698cb4d5dd3cbd4dce21
Jose Luis Koller <joseluiskoller98@gmail.com>
2021-08-18 09:58:42 -0300

    Se agregaron las paginas de NotFound y AboutUs.

--- src/pages/aboutUs/aboutUs.js
+++ src/pages/aboutUs/aboutUs.js
@@ -1,5 +1,45 @@
+import { Col, Row } from "react-bootstrap";
+import "./aboutUs.scss";
+
 function AboutUs() {
-  return <h1>Acerca de Nosotros</h1>;
+  return (
+    <main id="aboutUs">
+      <h1>¿Quienes somos?</h1>
+      <Row>
+        <Col id="usText">
+          <p>
+            {" "}
+            Nos conocimos en la secundaria, empezamos siendo amigos sin saber
+            que todos guardabamos en secreto una pasion por el Anime. Cuando
+            estabamos el ultimo año, a uno de nosotros se le escapo el secreto,
+            y ahi todo florecio, no podiamos creer que compartiamos semejante
+            pasion y por casi los mismos animes.
+          </p>
+          <p>
+            {" "}
+            Luego de varios años, logramos empezar con este sueño que no es solo
+            nuestro sino de muchas otras personas que buscan cosas respecto al
+            tema y no encuentran.
+          </p>
+          <p className="text-muted">
+            Aca te dejamos una de nuestras escenas favoritas del mundo del
+            Anime. ¡Larga vida al Comandante Erwin!
+          </p>
+        </Col>
+        <Col id="video">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube-nocookie.com/embed/YT6cguwIJTU"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </Col>
+      </Row>
+    </main>
+  );
 }
 
 export default AboutUs;
--- src/pages/notFound/notFound.js
+++ src/pages/notFound/notFound.js
@@ -1,7 +1,21 @@
 import "./notFound.scss";
+import test from "../../assets/not-found.png";
+import { Link } from "react-router-dom";
 
 function NotFound() {
-  return <main>No encontrado</main>;
+  return (
+    <main id="notFound">
+      <img src={test} alt="L mirando fijamente tu alma" id="LImg" />
+      <h1>Em.. Esto es embarazoso</h1>
+      <p>
+        No logramos encontrar la pagina que buscabas, te invitamos a volver al
+        inicio.
+      </p>
+      <Link to="/" id="backHome">
+        Volvamos a la normalidad 😁
+      </Link>
+    </main>
+  );
 }
 
 export default NotFound;
